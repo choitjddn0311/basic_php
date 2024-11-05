137947ca5ab70d1fa707394e245aacb1cc008e0d
Sungwoo Choi <128563032+choitjddn0311@users.noreply.github.com>
2024-11-05 23:39:33 +0900

    feat: js grammer

--- grammer/1.php
+++ grammer/1.php
@@ -6,7 +6,18 @@
     <title>post 전송</title>
 </head>
 <body>
-    <form action="1sub.php" method="POST">
+    <script>
+        function one() {
+            let txt1 = document.querySelector('.txt1');
+            let txt2 = document.querySelector('.txt2');
+            if (txt1.value == '' || txt2.value == '') {
+                alert('입력해야할 값이 비어있습니다');
+                return false;  // 오타 수정: flase -> false
+            }
+            return true;
+        }
+    </script>
+    <form action="1sub.php" method="POST" onsubmit="return one()">
         <input type="text" name="txt1" class="txt1">
         <input type="text" name="txt2" class="txt2">
         <input type="submit" value="제출">
--- grammer/1sub.php
+++ grammer/1sub.php
@@ -1,5 +1,6 @@
 <?php
     $txt1 = $_POST['txt1'];
     $txt2 = $_POST['txt2'];
-    echo $txt1;
+        echo $txt1;
+        echo "<a>$txt2</a>";
 ?>
