72ea01844d0908818480b35547daf7bfdea8e329
choitjddn0311 <cswcjw311427@naver.com>
2024-11-05 20:00:03 +0900

    feat: post method

---
+++ grammer/1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>post 전송</title>
+</head>
+<body>
+    <form action="1sub.php" method="POST">
+        <input type="text" name="txt1" class="txt1">
+        <input type="text" name="txt2" class="txt2">
+        <input type="submit" value="제출">
+    </form>
+</body>
+</html>
---
+++ grammer/1sub.php
@@ -0,0 +1,5 @@
+<?php
+    $txt1 = $_POST['txt1'];
+    $txt2 = $_POST['txt2'];
+    echo $txt1;
+?>
