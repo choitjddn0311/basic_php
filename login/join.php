0063db47890b40d6e410969e3824556b6a8e48cf
Sungwoo Choi <128563032+choitjddn0311@users.noreply.github.com>
2024-11-13 00:05:56 +0900

    feat: file create

--- login/join.php
+++ login/join.php
@@ -20,54 +20,29 @@
         <?php
             $conn = mysqli_connect('localhost' , 'root' , '' , 'user_account');
 
-            if($_SERVER["REQUEST_METHOD"] == "POST"){
-                $user_id = $_POST['id'];
-                $user_pw = $_POST['pw'];
+            if($_SERVER['REQUEST_METHOD'] == "POST"){
+                $user_id = mysqli_real_escape_string($conn,$_POST['id']);
+                $user_pw = mysqli_real_escape_string($conn, $_POST['pw']);
+                
+                $insert = "INSERT INTO joinData(id,password) VALUES('$user_id' ,  '$user_pw')";
 
-                $insert = "INSERT INTO joinData(id,password) VALUES('$user_id' , '$user_pw')";
-
-                if(mysqli_query($conn,$insert)){
+                if(mysqli_query($conn , $insert)){
                     echo "
                     <script>
-                        alert('join complete');
-                        location.href = 'login.php';
+                        alert('join complete!');
+                        location.href = 'login.php'
                     </script>
                     ";
                 } else {
                     echo "
                     <script>
                         alert('join fail');
+
                     </script>
                     ";
                 }
             }
         ?>
     </main>
-
-    <script>
-        const id = document.querySelector('.id');
-        const pw = document.querySelector('.pw');
-        const pwCheck = document.querySelector('.pwCheck');
-        const joinBtn = document.querySelector('.joinBtn');
-
-        joinBtn.addEventListener('click',(e) => {
-            if(id.value.trim() === ''){
-                alert('please input your id');
-                e.preventDefault();
-                return;
-            }
-            if(pw.value.trim() === '' || pwCheck.value.trim() === ''){
-                alert('please input your password');
-                e.preventDefault();
-                return;
-            }
-
-            if(pw.value !== pwCheck.value){
-                alert('the password is not correct');
-                e.preventDefault();
-                return;
-            }
-        })
-    </script>
 </body>
 </html>
